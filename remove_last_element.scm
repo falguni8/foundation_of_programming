9077ea63d574e11f53f08a0fd6699395727d6478
falguni8 <89798656+falguni8@users.noreply.github.com>
2022-08-27 01:37:14 +0530

    Update remove_last_element.scm

--- remove_last_element.scm
+++ remove_last_element.scm
@@ -1,6 +1,18 @@
+//non empty list
+
 (define (fun lst)
     (cond ( (null? (cdr lst)) '()  )
     (else (cons (car lst) ( fun (cdr lst) )) )
     )
 )
 (display (fun '(1 2 3 4)) )
+
+
+///list
+(define (fun lst)
+    (cond ((null? lst) 'emptylist )
+    ( (null? (cdr lst)) '()  )
+    (else (cons (car lst) ( fun (cdr lst) )) )
+    )
+)
+(display (fun '()) )
